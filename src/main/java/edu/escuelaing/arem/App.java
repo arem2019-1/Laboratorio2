d2e8da10a1685f3653de245259e150f288acd73f
cesareduardo824 <cesareduardo824@gmail.com>
2019-01-22 10:11:57 -0500

    Proyecto COmpleto.

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -1,13 +1,24 @@
 package edu.escuelaing.arem;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello Heroku");
+        System.out.println("Hello World!");
     }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set
+        //(i.e.on localhost)
+    }
+
 }

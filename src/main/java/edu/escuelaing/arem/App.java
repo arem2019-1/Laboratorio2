b3254706eefb5cf18aa7290da65e374df952cc01
estudiante <estudiante@linux-2.labinfo.is.escuelaing.edu.co>
2019-01-22 10:44:24 -0500

    Corregido

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -9,6 +9,7 @@ import static spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
         System.out.println("Hello World!");
     }
